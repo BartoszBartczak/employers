5dff4e637bb022bb76c522f3cfda8b8102c51c12
BartoszBartczak <intermarism@gmail.com>
2021-10-11 09:20:22 +0200

    Bootstrap instalation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './core/employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeesComponent },
+  { path: '', redirectTo: '/employees', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
